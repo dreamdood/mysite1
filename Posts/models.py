722104a6b7cb3673a70b5ed0f34dd498b4a004fc
Michael Legere <dreamdood.pdf@gmail.com>
2020-10-06 14:37:51 -0400

    Chnages to models

--- Posts/models.py
+++ Posts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Post(models.Model):
+class post(models.Model):
     title = models.CharField(max_length=100, null=False, blank=True)
     body = models.TextField(null=False, blank=True)
     image = models.ImageField
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.utils import timezone
-
-from django.db import models
 
 # Create your models here.
 
